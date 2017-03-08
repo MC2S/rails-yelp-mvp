7bdb02fc3633fb92737ff1f6e6f608470d28f736
Simon De Swarte <simon.de-swarte@hotmail.fr>
2017-03-08 13:11:59 +0100

    Basic methods added to RestaurantsController

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,22 +1,43 @@
 class RestaurantsController < ApplicationController
+    before_action :find_restaurant, only: [:show, :edit, :update, :destroy]
+
   def index
+    @restaurants = Restaurant.all
   end
 
   def show
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
+    Restaurant.new(restaurant_params).save
+    redirect_to_restaurants_path
   end
 
   def edit
   end
 
   def update
+    @restaurant.update(task_params)
+    redirect_to_restaurants_path
   end
 
   def destroy
+    @restaurant.delete
+    redirect_to_restaurants_path
   end
+
+private
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,6 @@
 class Restaurant < ApplicationRecord
+  validates :name, uniqueness: true, presence: true
+  validates :address, presence: true
+  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"], allow_nil: false }
+  validates :phone_number, format: { with: /\A(0|\+33)[1-9]([-. ]?[0-9]{2}){4}\z/ }
 end
